22f13f136275ebd10ddfb04a2c9c88f0e469056a
Aamos Pernu <aamospernu@AamosMac.nekonet.fi>
2020-10-13 13:01:06 +0300

    Removed unnecessary files

--- src/include/sys/mem.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MEM_H_
-#define _MEM_H_
-
-#include <sys/types.h>
-
-void* memset(void* s, int c, size_t n);
-void* memcpy(void* dest, const void* src, size_t n);
-
-#endif
---
+++ src/include/timer.h
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2020 Aamos Pernu
+ * 
+ * AamOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * AamOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this aamOS.  If not, see <https://www.gnu.org/licenses/>.
+ ******************************************************************************/
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+void set_timer_freq(uint32_t hz);
+void timer(registers_t *regs);
+void timer_init(void);
+void sleep(uint32_t d);
+
+#endif
